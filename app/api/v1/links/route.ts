e5b79e8ff0de22f86387a46c2155044adeb19fc4
MohamedBassem <me@mbassem.com>
2024-02-06 01:17:29 +0000

    [API] Add the GET /api/v1/links api

--- app/api/v1/links/route.ts
+++ app/api/v1/links/route.ts
@@ -26,3 +26,27 @@ export async function POST(request: NextRequest) {
 
     return NextResponse.json(link, { status: 201 });
 }
+
+export async function GET() {
+    // TODO: We probably should be using an API key here instead of the session;
+    const session = await getServerSession(authOptions);
+    if (!session) {
+        return new Response(null, { status: 401 });
+    }
+    const links = await prisma.bookmarkedLink.findMany({
+        where: {
+            userId: session.user.id,
+        },
+        include: {
+            details: {
+                select: {
+                    title: true,
+                    description: true,
+                    imageUrl: true,
+                }
+            },
+        }
+    })
+
+    return NextResponse.json({links});
+}
