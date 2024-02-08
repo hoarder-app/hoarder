c5bfa5000f178475d0b019b5a960916134b2ecfb
MohamedBassem <me@mbassem.com>
2024-02-08 17:36:31 +0000

    [build] Migrating to yarn 2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,33 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "next/core-web-vitals",
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "plugin:react-hooks/recommended",
-        "plugin:@next/next/recommended",
-        "next",
-        "prettier"
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "next/core-web-vitals",
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:@next/next/recommended",
+    "next",
+    "prettier",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  ignorePatterns: ["postcss.config.js"],
+  rules: {
+    "no-redeclare": "off",
+    "@next/next/no-html-link-for-pages": "off",
+    "no-undef": "off",
+    "react/jsx-no-undef": "off",
+    "no-unused-vars": [
+      "error",
+      {
+        varsIgnorePattern: "^_",
+        argsIgnorePattern: "^_",
+      },
     ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "ignorePatterns": ["postcss.config.js"],
-    "rules": {
-        "no-redeclare": "off",
-        "@next/next/no-html-link-for-pages": "off",
-        "no-undef": "off",
-        "react/jsx-no-undef": "off",
-        "no-unused-vars": [
-            "error",
-            {
-                "varsIgnorePattern": "^_",
-                "argsIgnorePattern": "^_"
-            }
-        ]
-    }
-}
+  },
+};
