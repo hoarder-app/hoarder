a86156d805031f0fa3b131dc4fac5a7b10deb66f
MohamedBassem <me@mbassem.com>
2024-02-10 03:05:29 +0000

    chore: Enable prisma logging in dev env

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,6 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({
+    log:
+        process.env.NODE_ENV === "development"
+            ? ["query", "error", "warn"]
+            : ["error"],
+});
 
 // For some weird reason accessing @prisma/client from any package is causing problems (specially in error handling).
 // Re export them here instead.
