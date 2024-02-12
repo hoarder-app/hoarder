e2bdccd483677cd60a92f4b0308bd2e4a0c02bfb
MohamedBassem <me@mbassem.com>
2024-02-12 18:43:48 +0000

    Add the scafolding for the browser extension

---
+++ packages/browser-extension/src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+      <div className="text-3xl">Test</div>
+    </>
+  );
+}
+
+export default App;
---
+++ packages/browser-extension/vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { crx } from "@crxjs/vite-plugin";
+import manifest from "./manifest.json";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), crx({ manifest })],
+});
