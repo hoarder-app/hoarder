8dfcaa372382f70da5e20d9c8fd65392424e3cfc
MohamedBassem <me@mbassem.com>
2024-02-13 21:33:00 +0000

    feature: Add error and not found boundaries in the dashboard

---
+++ packages/web/app/dashboard/error.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Error() {
+    return (
+        <div className="flex size-full">
+            <div className="m-auto text-3xl">Something went wrong</div>
+        </div>
+    );
+}
