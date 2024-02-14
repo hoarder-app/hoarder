dde49dd60e00653960223b8dcea3488b7845d43b
MohamedBassem <me@mbassem.com>
2024-02-14 00:30:58 +0000

    feature: Sort tags by usage and show usage numbers

--- packages/web/app/dashboard/tags/page.tsx
+++ packages/web/app/dashboard/tags/page.tsx
@@ -1,8 +1,21 @@
-import { Button } from "@/components/ui/button";
 import { getServerAuthSession } from "@/server/auth";
 import { prisma } from "@remember/db";
 import Link from "next/link";
 import { redirect } from "next/navigation";
+
+function TagPill({ name, count }: { name: string; count: number }) {
+  return (
+    <Link
+      className="mx-1.5 my-1 flex rounded-lg bg-gray-600 hover:bg-gray-700"
+      href={`/dashboard/tags/${name}`}
+    >
+      <span className="p-1.5 text-gray-200">{name}</span>
+      <span className="rounded-r-lg bg-gray-300 p-1.5 text-gray-600">
+        {count}
+      </span>
+    </Link>
+  );
+}
 
 export default async function TagsPage() {
   const session = await getServerAuthSession();
@@ -14,7 +27,17 @@ export default async function TagsPage() {
     where: {
       userId: session.user.id,
     },
+    include: {
+      _count: {
+        select: {
+          bookmarks: true,
+        },
+      },
+    },
   });
+
+  // Sort tags by usage desc
+  tags.sort((a, b) => b._count.bookmarks - a._count.bookmarks);
 
   return (
     <div className="container mt-2 space-y-3">
@@ -22,13 +45,7 @@ export default async function TagsPage() {
       <hr />
       <div className="flex flex-wrap">
         {tags.map((t) => (
-          <Link
-            className="m-1 block min-w-16 rounded-xl bg-black p-2 text-center text-white"
-            key={t.id}
-            href={`/dashboard/tags/${t.name}`}
-          >
-            {t.name}
-          </Link>
+          <TagPill key={t.id} name={t.name} count={t._count.bookmarks} />
         ))}
       </div>
     </div>
