5a3fd2d7573c62467a40c919244d12b468458a38
MohamedBassem <me@mbassem.com>
2024-02-17 12:39:54 +0000

    ui: Add bookmark card skeleton

---
+++ packages/web/app/dashboard/bookmarks/components/BookmarkCardSkeleton.tsx
@@ -0,0 +1,32 @@
+import {
+  ImageCard,
+  ImageCardBody,
+  ImageCardContent,
+  ImageCardFooter,
+  ImageCardTitle,
+  ImageCardBanner,
+} from "@/components/ui/imageCard";
+import { Skeleton } from "@/components/ui/skeleton";
+
+export default function BookmarkCardSkeleton() {
+  return (
+    <ImageCard
+      className={
+        "border-grey-100 border bg-gray-50 duration-300 ease-in hover:border-blue-300 hover:transition-all"
+      }
+    >
+      <ImageCardBanner src="/blur.avif" />
+      <ImageCardContent>
+        <ImageCardTitle>
+        </ImageCardTitle>
+        <ImageCardBody className="space-y-2">
+          <Skeleton className="h-4 w-full" />
+          <Skeleton className="h-4 w-full" />
+          <Skeleton className="h-4 w-full" />
+        </ImageCardBody>
+        <ImageCardFooter>
+        </ImageCardFooter>
+      </ImageCardContent>
+    </ImageCard>
+  );
+}
