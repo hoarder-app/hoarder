a942e69c9a830142c7ead9cbf7c1aa67fd87bf38
MohamedBassem <me@mbassem.com>
2024-03-02 09:33:28 +0000

    dev: Disable db query logging given that it's very noisy

--- packages/db/drizzle.ts
+++ packages/db/drizzle.ts
@@ -4,20 +4,9 @@ import Database from "better-sqlite3";
 import * as schema from "./schema";
 import { migrate } from "drizzle-orm/better-sqlite3/migrator";
 import path from "path";
-import { Logger } from "drizzle-orm";
-
-class MyLogger implements Logger {
-  logQuery(query: string, _params: unknown[]): void {
-    if (process.env.NODE_ENV === "production") {
-      return;
-    }
-    const line = `Query: ${query}`;
-    console.log(line);
-  }
-}
 
 const sqlite = new Database(process.env.DATABASE_URL);
-export const db = drizzle(sqlite, { schema, logger: new MyLogger() });
+export const db = drizzle(sqlite, { schema });
 
 export function getInMemoryDB(runMigrations: boolean) {
   const mem = new Database(":memory:");
