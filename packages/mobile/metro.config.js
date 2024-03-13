04572a8e5081b1e4871e273cde9dbaaa44c52fe0
MohamedBassem <me@mbassem.com>
2024-03-13 21:43:44 +0000

    structure: Create apps dir and copy tooling dir from t3-turbo repo

--- packages/mobile/metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-const { withNativeWind } = require("nativewind/metro");
-
-/** @type {import('expo/metro-config').MetroConfig} */
-// eslint-disable-next-line no-undef
-const config = getDefaultConfig(__dirname);
-
-module.exports = withNativeWind(config, { input: "./globals.css" });
