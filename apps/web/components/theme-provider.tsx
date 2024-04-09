5ab6c3304b4a2d055767b8195fac9c9eec776d16
MohamedBassem <me@mbassem.com>
2024-04-09 16:32:41 +0100

    fix(web): Fix the toggle theme button allowing clicks from anywhere in the dropdown item

--- apps/web/components/theme-provider.tsx
+++ apps/web/components/theme-provider.tsx
@@ -2,8 +2,17 @@
 
 import type { ThemeProviderProps } from "next-themes/dist/types";
 import * as React from "react";
-import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ThemeProvider as NextThemesProvider, useTheme } from "next-themes";
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
+
+export function useToggleTheme() {
+  const { theme, setTheme } = useTheme();
+  if (theme == "dark") {
+    return () => setTheme("light");
+  } else {
+    return () => setTheme("dark");
+  }
 }
