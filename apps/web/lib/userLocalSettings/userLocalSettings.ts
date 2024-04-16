bb44ebcb9967bde81d15e2f7858d515777681c10
MohamedBassem <me@mbassem.com>
2024-04-16 19:20:11 +0100

    fix: Fix user local settings cookie to have max age as by default it's a session cookie. Fix #104

--- apps/web/lib/userLocalSettings/userLocalSettings.ts
+++ apps/web/lib/userLocalSettings/userLocalSettings.ts
@@ -11,8 +11,10 @@ import {
 export async function updateBookmarksLayout(layout: BookmarksLayoutTypes) {
   const userSettings = cookies().get(USER_LOCAL_SETTINGS_COOKIE_NAME);
   const parsed = parseUserLocalSettings(userSettings?.value);
-  cookies().set(
-    USER_LOCAL_SETTINGS_COOKIE_NAME,
-    JSON.stringify({ ...parsed, bookmarkGridLayout: layout }),
-  );
+  cookies().set({
+    name: USER_LOCAL_SETTINGS_COOKIE_NAME,
+    value: JSON.stringify({ ...parsed, bookmarkGridLayout: layout }),
+    maxAge: 34560000, // Chrome caps max age to 400 days
+    sameSite: "lax",
+  });
 }
