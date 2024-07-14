888dad6ec2f3c78676422ad503dcf7ff81507552
kamtschatka <simon.schatka@gmx.at>
2024-07-15 01:11:27 +0200

    feature: Add github markdown support which includes tables, checklists and auto links #300 (#302)
    added the possibility to add checkboxes

--- apps/web/components/ui/markdown-component.tsx
+++ apps/web/components/ui/markdown-component.tsx
@@ -4,6 +4,7 @@ import { cn } from "@/lib/utils";
 import Markdown from "react-markdown";
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
 import { dracula } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import remarkGfm from "remark-gfm";
 
 function PreWithCopyBtn({ className, ...props }: React.ComponentProps<"pre">) {
   const ref = React.useRef<HTMLPreElement>(null);
@@ -27,6 +28,7 @@ export function MarkdownComponent({
 }) {
   return (
     <Markdown
+      remarkPlugins={[remarkGfm]}
       className="prose dark:prose-invert"
       components={{
         pre({ ...props }) {
