631d3894069163e7a7e16d3ac482137b7efccfd2
Marc <github@wuast24.de>
2024-09-18 06:13:11 +0000

    Remove meili healthcheck

--- apps/web/app/api/health/route.ts
+++ apps/web/app/api/health/route.ts
@@ -1,43 +1,9 @@
 import { NextRequest, NextResponse } from "next/server";
-import fetch from "node-fetch";
-
-import serverConfig from "../../../../../packages/shared/config";
-
-const checkMeilisearchHealth = async () => {
-  if (!serverConfig.meilisearch) {
-    return { status: "unavailable", message: "Meilisearch is not configured" };
-  }
-
-  try {
-    const response = await fetch(`${serverConfig.meilisearch.address}/health`, {
-      headers: {
-        Authorization: `Bearer ${serverConfig.meilisearch.key}`,
-      },
-    });
-
-    if (response.ok) {
-      return { status: "ok", message: "Meilisearch is working" };
-    } else {
-      return {
-        status: "error",
-        message: "Meilisearch is not responding correctly",
-      };
-    }
-  } catch (error) {
-    let errorMessage = "An unknown error occurred";
-    if (error instanceof Error) {
-      errorMessage = `Meilisearch check failed: ${error.message}`;
-    }
-    return { status: "error", message: errorMessage };
-  }
-};
 
 export const GET = async (_req: NextRequest) => {
   const webAppStatus = { status: "ok", message: "Web app is working" };
-  const meilisearchStatus = await checkMeilisearchHealth();
 
   return NextResponse.json({
     webApp: webAppStatus,
-    meilisearch: meilisearchStatus,
   });
 };
