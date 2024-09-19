f3d69ddd15b41e203b835cd2ae88b0e09bd306ee
Marc <github@wuast24.de>
2024-09-20 00:10:19 +0200

    Replace unnecessary variable with static content

--- apps/web/app/api/health/route.ts
+++ apps/web/app/api/health/route.ts
@@ -1,9 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (_req: NextRequest) => {
-  const webAppStatus = { status: "ok", message: "Web app is working" };
-
   return NextResponse.json({
-    webApp: webAppStatus,
+    status: "ok",
+    message: "Web app is working",
   });
 };
