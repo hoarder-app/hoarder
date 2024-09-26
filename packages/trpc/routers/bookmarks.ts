bab891f97c31200752dfaf386406f5a9b1c588e1
MohamedBassem <me@mbassem.com>
2024-09-26 20:57:13 +0000

    Add validation on the input of update tags to ensure that its not empty

--- packages/trpc/routers/bookmarks.ts
+++ packages/trpc/routers/bookmarks.ts
@@ -678,18 +678,28 @@ export const bookmarksAppRouter = router({
       z.object({
         bookmarkId: z.string(),
         attach: z.array(
-          z.object({
-            // At least one of the two must be set
-            tagId: z.string().optional(), // If the tag already exists and we know its id we should pass it
-            tagName: z.string().optional(),
-          }),
+          z
+            .object({
+              // At least one of the two must be set
+              tagId: z.string().optional(), // If the tag already exists and we know its id we should pass it
+              tagName: z.string().optional(),
+            })
+            .refine((val) => !!val.tagId || !!val.tagName, {
+              message: "You must provide either a tagId or a tagName",
+              path: ["tagId", "tagName"],
+            }),
         ),
         detach: z.array(
-          z.object({
-            // At least one of the two must be set
-            tagId: z.string().optional(),
-            tagName: z.string().optional(), // Also allow removing by tagName, to make CLI usage easier
-          }),
+          z
+            .object({
+              // At least one of the two must be set
+              tagId: z.string().optional(),
+              tagName: z.string().optional(), // Also allow removing by tagName, to make CLI usage easier
+            })
+            .refine((val) => !!val.tagId || !!val.tagName, {
+              message: "You must provide either a tagId or a tagName",
+              path: ["tagId", "tagName"],
+            }),
         ),
       }),
     )
@@ -767,42 +777,40 @@ export const bookmarksAppRouter = router({
             .returning();
         }
 
-        let allIds: string[] = [];
-
         // If there is nothing to add, the "or" statement will become useless and
         // the query below will simply select all the existing tags for this user and assign them to the bookmark
-        if (toAddTagNames.length > 0 || toAddTagIds.length > 0) {
-          allIds = (
-            await tx.query.bookmarkTags.findMany({
-              where: and(
-                eq(bookmarkTags.userId, ctx.user.id),
-                or(
-                  toAddTagIds.length > 0
-                    ? inArray(bookmarkTags.id, toAddTagIds)
-                    : undefined,
-                  toAddTagNames.length > 0
-                    ? inArray(bookmarkTags.name, toAddTagNames)
-                    : undefined,
-                ),
+        invariant(toAddTagNames.length > 0 || toAddTagIds.length > 0);
+        const allIds = (
+          await tx.query.bookmarkTags.findMany({
+            where: and(
+              eq(bookmarkTags.userId, ctx.user.id),
+              or(
+                toAddTagIds.length > 0
+                  ? inArray(bookmarkTags.id, toAddTagIds)
+                  : undefined,
+                toAddTagNames.length > 0
+                  ? inArray(bookmarkTags.name, toAddTagNames)
+                  : undefined,
               ),
-              columns: {
-                id: true,
-              },
-            })
-          ).map((t) => t.id);
+            ),
+            columns: {
+              id: true,
+            },
+          })
+        ).map((t) => t.id);
 
-          await tx
-            .insert(tagsOnBookmarks)
-            .values(
-              allIds.map((i) => ({
-                tagId: i,
-                bookmarkId: input.bookmarkId,
-                attachedBy: "human" as const,
-                userId: ctx.user.id,
-              })),
-            )
-            .onConflictDoNothing();
-        }
+        await tx
+          .insert(tagsOnBookmarks)
+          .values(
+            allIds.map((i) => ({
+              tagId: i,
+              bookmarkId: input.bookmarkId,
+              attachedBy: "human" as const,
+              userId: ctx.user.id,
+            })),
+          )
+          .onConflictDoNothing();
+
         await triggerSearchReindex(input.bookmarkId);
         return {
           bookmarkId: input.bookmarkId,
