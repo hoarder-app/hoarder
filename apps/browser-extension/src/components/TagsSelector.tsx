fb73d50aa0c0ffdab9c96a491641dec78d9a38cd
kamtschatka <simon.schatka@gmx.at>
2024-10-16 18:30:32 +0200

    nested Lists do not fit well in browser extension window #535 increased size to have the same size as the input field that triggers it

--- apps/browser-extension/src/components/TagsSelector.tsx
+++ apps/browser-extension/src/components/TagsSelector.tsx
@@ -64,7 +64,7 @@ export function TagsSelector({ bookmarkId }: { bookmarkId: string }) {
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
+      <PopoverContent className="w-[320px] p-0">
         <Command>
           <CommandInput
             value={input}
