e89a38680532c3ab72ef26dfe88bb64476b709ab
Mohamed Bassem <me@mbassem.com>
2024-10-20 15:54:34 +0000

    feature(api): Add REST APIs to update bookmarks, tags and lists

--- apps/web/app/api/v1/lists/[listId]/route.ts
+++ apps/web/app/api/v1/lists/[listId]/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest } from "next/server";
 import { buildHandler } from "@/app/api/v1/utils/handler";
 
+import { zNewBookmarkListSchema } from "@hoarder/shared/types/lists";
+
 export const dynamic = "force-dynamic";
 
 export const GET = (
@@ -17,6 +19,22 @@ export const GET = (
         status: 200,
         resp: list,
       };
+    },
+  });
+
+export const PATCH = (
+  req: NextRequest,
+  { params }: { params: { listId: string } },
+) =>
+  buildHandler({
+    req,
+    bodySchema: zNewBookmarkListSchema.partial(),
+    handler: async ({ api, body }) => {
+      const list = await api.lists.edit({
+        listId: params.listId,
+        ...body!,
+      });
+      return { status: 200, resp: list };
     },
   });
 
--- apps/web/app/api/v1/tags/[tagId]/route.ts
+++ apps/web/app/api/v1/tags/[tagId]/route.ts
@@ -1,6 +1,8 @@
 import { NextRequest } from "next/server";
 import { buildHandler } from "@/app/api/v1/utils/handler";
 
+import { zUpdateTagRequestSchema } from "@hoarder/shared/types/tags";
+
 export const dynamic = "force-dynamic";
 
 export const GET = (
@@ -17,6 +19,22 @@ export const GET = (
         status: 200,
         resp: tag,
       };
+    },
+  });
+
+export const PATCH = (
+  req: NextRequest,
+  { params }: { params: { tagId: string } },
+) =>
+  buildHandler({
+    req,
+    bodySchema: zUpdateTagRequestSchema.omit({ tagId: true }),
+    handler: async ({ api, body }) => {
+      const tag = await api.tags.update({
+        tagId: params.tagId,
+        ...body!,
+      });
+      return { status: 200, resp: tag };
     },
   });
 
