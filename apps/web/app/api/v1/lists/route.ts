10dcf2e7429601fcbde42e3d10c46c074dc9a28a
Mohamed Bassem <me@mbassem.com>
2024-10-20 14:58:16 +0000

    feature: Add APIs to create new lists and bookmarks

--- apps/web/app/api/v1/lists/route.ts
+++ apps/web/app/api/v1/lists/route.ts
@@ -1,5 +1,7 @@
 import { NextRequest } from "next/server";
 
+import { zNewBookmarkListSchema } from "@hoarder/shared/types/lists";
+
 import { buildHandler } from "../utils/handler";
 
 export const dynamic = "force-dynamic";
@@ -10,5 +12,15 @@ export const GET = (req: NextRequest) =>
     handler: async ({ api }) => {
       const lists = await api.lists.list();
       return { status: 200, resp: lists };
+    },
+  });
+
+export const POST = (req: NextRequest) =>
+  buildHandler({
+    req,
+    bodySchema: zNewBookmarkListSchema,
+    handler: async ({ api, body }) => {
+      const list = await api.lists.create(body!);
+      return { status: 201, resp: list };
     },
   });
--- packages/shared/types/lists.ts
+++ packages/shared/types/lists.ts
@@ -1,5 +1,14 @@
 import { z } from "zod";
 
+export const zNewBookmarkListSchema = z.object({
+  name: z
+    .string()
+    .min(1, "List name can't be empty")
+    .max(40, "List name is at most 40 chars"),
+  icon: z.string(),
+  parentId: z.string().nullish(),
+});
+
 export const zBookmarkListSchema = z.object({
   id: z.string(),
   name: z.string(),
