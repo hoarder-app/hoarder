70d049925a7982b82cca6c087eca7d79abfad51b
Mohamed Bassem <me@mbassem.com>
2024-12-22 13:37:14 +0000

    Fix svg errors

--- apps/web/components/dashboard/sidebar/ModileSidebar.tsx
+++ apps/web/components/dashboard/sidebar/ModileSidebar.tsx
@@ -1,5 +1,6 @@
+import Image from "next/image";
 import MobileSidebarItem from "@/components/shared/sidebar/ModileSidebarItem";
-import HoarderLogoIcon from "@/public/icons/logo-icon.svg";
+import hoarderLogoIcon from "@/public/icons/logo-icon.svg";
 import { ClipboardList, Search, Tag } from "lucide-react";
 
 export default async function MobileSidebar() {
@@ -7,7 +8,14 @@ export default async function MobileSidebar() {
     <aside className="w-full">
       <ul className="flex justify-between space-x-2 border-b-black px-5 py-2 pt-5">
         <MobileSidebarItem
-          logo={<HoarderLogoIcon className="w-5 fill-foreground" />}
+          logo={
+            <Image
+              alt="Hoarder Logo"
+              // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+              src={hoarderLogoIcon}
+              className="w-5 fill-foreground"
+            />
+          }
           path="/dashboard/bookmarks"
         />
         <MobileSidebarItem logo={<Search />} path="/dashboard/search" />
