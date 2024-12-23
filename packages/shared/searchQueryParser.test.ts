71b735a85ec8cadc9854ee535142b37854634483
Mohamed Bassem <me@mbassem.com>
2024-12-23 13:29:56 +0000

    Add ability to interleve text

--- packages/shared/searchQueryParser.test.ts
+++ packages/shared/searchQueryParser.test.ts
@@ -5,142 +5,228 @@ import { parseSearchQuery } from "./searchQueryParser";
 describe("Search Query Parser", () => {
   test("simple is queries", () => {
     expect(parseSearchQuery("is:archived")).toEqual({
-      type: "archived",
-      archived: true,
+      text: "",
+      matcher: {
+        type: "archived",
+        archived: true,
+      },
     });
     expect(parseSearchQuery("is:not_archived")).toEqual({
-      type: "archived",
-      archived: false,
+      text: "",
+      matcher: {
+        type: "archived",
+        archived: false,
+      },
     });
     expect(parseSearchQuery("is:fav")).toEqual({
-      type: "favourited",
-      favourited: true,
+      text: "",
+      matcher: {
+        type: "favourited",
+        favourited: true,
+      },
     });
     expect(parseSearchQuery("is:not_fav")).toEqual({
-      type: "favourited",
-      favourited: false,
+      text: "",
+      matcher: {
+        type: "favourited",
+        favourited: false,
+      },
     });
   });
 
   test("simple string queries", () => {
     expect(parseSearchQuery("url:https://example.com")).toEqual({
-      type: "url",
-      url: "https://example.com",
+      text: "",
+      matcher: {
+        type: "url",
+        url: "https://example.com",
+      },
     });
     expect(parseSearchQuery('url:"https://example.com"')).toEqual({
-      type: "url",
-      url: "https://example.com",
+      text: "",
+      matcher: {
+        type: "url",
+        url: "https://example.com",
+      },
     });
     expect(parseSearchQuery("#my-tag")).toEqual({
-      type: "tagName",
-      tagName: "my-tag",
+      text: "",
+      matcher: {
+        type: "tagName",
+        tagName: "my-tag",
+      },
     });
     expect(parseSearchQuery('#"my tag"')).toEqual({
-      type: "tagName",
-      tagName: "my tag",
+      text: "",
+      matcher: {
+        type: "tagName",
+        tagName: "my tag",
+      },
     });
     expect(parseSearchQuery("list:my-list")).toEqual({
-      type: "listName",
-      listName: "my-list",
+      text: "",
+      matcher: {
+        type: "listName",
+        listName: "my-list",
+      },
     });
     expect(parseSearchQuery('list:"my list"')).toEqual({
-      type: "listName",
-      listName: "my list",
+      text: "",
+      matcher: {
+        type: "listName",
+        listName: "my list",
+      },
     });
   });
   test("date queries", () => {
     expect(parseSearchQuery("after:2023-10-12")).toEqual({
-      type: "dateAfter",
-      dateAfter: new Date("2023-10-12"),
+      text: "",
+      matcher: {
+        type: "dateAfter",
+        dateAfter: new Date("2023-10-12"),
+      },
     });
     expect(parseSearchQuery("before:2023-10-12")).toEqual({
-      type: "dateBefore",
-      dateBefore: new Date("2023-10-12"),
+      text: "",
+      matcher: {
+        type: "dateBefore",
+        dateBefore: new Date("2023-10-12"),
+      },
     });
   });
 
   test("complex queries", () => {
     expect(parseSearchQuery("is:fav is:archived")).toEqual({
-      type: "and",
-      matchers: [
-        {
-          type: "favourited",
-          favourited: true,
-        },
-        {
-          type: "archived",
-          archived: true,
-        },
-      ],
+      text: "",
+      matcher: {
+        type: "and",
+        matchers: [
+          {
+            type: "favourited",
+            favourited: true,
+          },
+          {
+            type: "archived",
+            archived: true,
+          },
+        ],
+      },
     });
 
     expect(parseSearchQuery("(is:fav is:archived) #my-tag")).toEqual({
-      type: "and",
-      matchers: [
-        {
-          type: "and",
-          matchers: [
-            {
-              type: "favourited",
-              favourited: true,
-            },
-            {
-              type: "archived",
-              archived: true,
-            },
-          ],
-        },
-        {
-          type: "tagName",
-          tagName: "my-tag",
-        },
-      ],
+      text: "",
+      matcher: {
+        type: "and",
+        matchers: [
+          {
+            type: "favourited",
+            favourited: true,
+          },
+          {
+            type: "archived",
+            archived: true,
+          },
+          {
+            type: "tagName",
+            tagName: "my-tag",
+          },
+        ],
+      },
     });
 
     expect(parseSearchQuery("(is:fav is:archived) or (#my-tag)")).toEqual({
-      type: "or",
-      matchers: [
-        {
-          type: "and",
-          matchers: [
-            {
-              type: "favourited",
-              favourited: true,
-            },
-            {
-              type: "archived",
-              archived: true,
-            },
-          ],
-        },
-        {
-          type: "tagName",
-          tagName: "my-tag",
-        },
-      ],
+      text: "",
+      matcher: {
+        type: "or",
+        matchers: [
+          {
+            type: "and",
+            matchers: [
+              {
+                type: "favourited",
+                favourited: true,
+              },
+              {
+                type: "archived",
+                archived: true,
+              },
+            ],
+          },
+          {
+            type: "tagName",
+            tagName: "my-tag",
+          },
+        ],
+      },
     });
 
     expect(parseSearchQuery("(is:fav or is:archived) and #my-tag")).toEqual({
-      type: "and",
-      matchers: [
-        {
-          type: "or",
-          matchers: [
-            {
-              type: "favourited",
-              favourited: true,
-            },
-            {
-              type: "archived",
-              archived: true,
-            },
-          ],
-        },
-        {
-          type: "tagName",
-          tagName: "my-tag",
-        },
-      ],
+      text: "",
+      matcher: {
+        type: "and",
+        matchers: [
+          {
+            type: "or",
+            matchers: [
+              {
+                type: "favourited",
+                favourited: true,
+              },
+              {
+                type: "archived",
+                archived: true,
+              },
+            ],
+          },
+          {
+            type: "tagName",
+            tagName: "my-tag",
+          },
+        ],
+      },
+    });
+  });
+  test("pure text", () => {
+    expect(parseSearchQuery("hello world")).toEqual({
+      text: "hello world",
+      matcher: undefined,
+    });
+  });
+
+  test("text interlived with matchers", () => {
+    expect(
+      parseSearchQuery(
+        "hello is:fav world is:archived mixed world #my-tag test",
+      ),
+    ).toEqual({
+      text: "hello world mixed world test",
+      matcher: {
+        type: "and",
+        matchers: [
+          {
+            type: "favourited",
+            favourited: true,
+          },
+          {
+            type: "archived",
+            archived: true,
+          },
+          {
+            type: "tagName",
+            tagName: "my-tag",
+          },
+        ],
+      },
+    });
+  });
+
+  test("unknown qualifiers are emitted as pure text", () => {
+    expect(parseSearchQuery("is:fav is:helloworld")).toEqual({
+      text: "is:helloworld",
+      matcher: {
+        type: "favourited",
+        favourited: true,
+      },
     });
   });
 });
--- packages/shared/searchQueryParser.ts
+++ packages/shared/searchQueryParser.ts
@@ -2,14 +2,17 @@ import {
   alt,
   alt_sc,
   apply,
-  buildLexer,
   expectEOF,
   expectSingleResult,
   kmid,
+  kright,
   lrec_sc,
   rule,
   seq,
+  str,
   tok,
+  Token,
+  TokenPosition,
 } from "typescript-parsec";
 import { z } from "zod";
 
@@ -19,141 +22,215 @@ enum TokenType {
   And = "AND",
   Or = "OR",
 
-  // Is Queries
-  Favourited = "FAVOURITED",
-  NotFavourited = "NOT_FAVOURITED",
-  Archived = "ARCHIVED",
-  NotArchived = "NOT_ARCHIVED",
-
-  // Filters
-  Url = "URL",
-  Tag = "TAG",
-  List = "LIST",
-  After = "AFTER",
-  Before = "BEFORE",
-  UrlPrefix = "URL_PREFIX",
-  TagPrefix = "TAG_PREFIX",
-  ListPrefix = "LIST_PREFIX",
-
+  Qualifier = "QUALIFIER",
+  Ident = "IDENT",
   StringLiteral = "STRING_LITERAL",
 
   LParen = "LPAREN",
   RParen = "RPAREN",
   Space = "SPACE",
+  Hash = "HASH",
 }
 
-const lexer = buildLexer([
-  [true, /^(and)/gi, TokenType.And],
-  [true, /^(or)/gi, TokenType.Or],
+class TokenImpl implements Token<TokenType> {
+  constructor(
+    private readonly lexer: Lexer,
+    private readonly input: string,
+    public kind: TokenType,
+    public text: string,
+    public pos: TokenPosition,
+  ) {}
 
-  // Is Queries
-  [true, /^is:fav/g, TokenType.Favourited],
-  [true, /^is:not_fav/g, TokenType.NotFavourited],
-  [true, /^is:archived/g, TokenType.Archived],
-  [true, /^is:not_archived/g, TokenType.NotArchived],
+  matchRule(rest: string, pos: number, regex: RegExp): string | undefined {
+    const res = regex.exec(rest);
+    if (!res) {
+      return undefined;
+    }
+    return res[0];
+  }
 
-  // String Filter
-  [true, /^url:/gi, TokenType.UrlPrefix],
-  [true, /^#/g, TokenType.TagPrefix],
-  [true, /^list:/gi, TokenType.ListPrefix],
+  public get next(): Token<TokenType> | undefined {
+    if (!this.input.length) {
+      return undefined;
+    }
 
-  [true, /^url:[^" \\) ]+/gi, TokenType.Url],
-  [true, /^#[^" \\) ]+/gi, TokenType.Tag],
-  [true, /^list:[^" \\)]+/gi, TokenType.List],
-  [true, /^list:[^" \\)]+/gi, TokenType.List],
-  [true, /^after:\d{4}-\d{2}-\d{2}/gi, TokenType.After],
-  [true, /^before:\d{4}-\d{2}-\d{2}/gi, TokenType.Before],
+    for (const [regex, tokenType] of this.lexer.lexerRules) {
+      const match = this.matchRule(this.input, this.pos.index, regex);
+      if (!match) {
+        continue;
+      }
+      return new TokenImpl(
+        this.lexer,
+        this.input.slice(match.length),
+        tokenType,
+        match,
+        {
+          index: this.pos.index + match.length,
+          columnBegin: this.pos.index + 1,
+          columnEnd: this.pos.index + 1 + match.length,
+          // Our strings are always only one line
+          rowBegin: 1,
+          rowEnd: 1,
+        },
+      );
+    }
+    // No match
+    throw new Error(
+      `Failed to tokenize the token at position ${this.pos.index}: ${this.input[0]}`,
+    );
+  }
+}
 
-  [true, /^"([^"]+)"/g, TokenType.StringLiteral],
+class Lexer {
+  // Rules are in order of priority
+  lexerRules: readonly [RegExp, TokenType][] = [
+    [/^and/i, TokenType.And],
+    [/^or/i, TokenType.Or],
 
-  [true, /^\(/g, TokenType.LParen],
-  [true, /^\)/g, TokenType.RParen],
-  [true, /^\s+/g, TokenType.Space],
-]);
+    [/^#/, TokenType.Hash],
+    [/^(is|url|list|after|before):/, TokenType.Qualifier],
 
-const MATCHER = rule<TokenType, Matcher>();
-const EXP = rule<TokenType, Matcher>();
+    [/^"([^"]+)"/, TokenType.StringLiteral],
+
+    [/^\(/, TokenType.LParen],
+    [/^\)/, TokenType.RParen],
+    [/^\s+/, TokenType.Space],
+
+    // This needs to be last as it matches a lot of stuff
+    [/^[^ ")(]+/, TokenType.Ident],
+  ] as const;
+  constructor() {
+    for (const [regex, _] of this.lexerRules) {
+      if (!regex.source.startsWith("^")) {
+        throw new Error("Regex must start with ^");
+      }
+    }
+  }
+
+  lex(query: string): Token<TokenType> | undefined {
+    const tok = new TokenImpl(
+      this,
+      query,
+      /* Doesn't matter */ TokenType.Ident,
+      "",
+      {
+        index: 0,
+        rowBegin: 1,
+        rowEnd: 1,
+        columnBegin: 0,
+        columnEnd: 0,
+      },
+    );
+    return tok.next;
+  }
+}
+
+export interface TextAndMatcher {
+  text: string;
+  matcher?: Matcher;
+}
+
+const MATCHER = rule<TokenType, TextAndMatcher>();
+const EXP = rule<TokenType, TextAndMatcher>();
 
 MATCHER.setPattern(
   alt_sc(
-    apply(
-      alt(
-        tok(TokenType.Favourited),
-        tok(TokenType.NotFavourited),
-        tok(TokenType.Archived),
-        tok(TokenType.NotArchived),
-      ),
-      (toks) => {
-        switch (toks.kind) {
-          case TokenType.Favourited:
-            return { type: "favourited", favourited: true };
-          case TokenType.NotFavourited:
-            return { type: "favourited", favourited: false };
-          case TokenType.Archived:
-            return { type: "archived", archived: true };
-          case TokenType.NotArchived:
-            return { type: "archived", archived: false };
-        }
-      },
-    ),
+    apply(kright(str("is:"), tok(TokenType.Ident)), (toks) => {
+      switch (toks.text) {
+        case "fav":
+          return {
+            text: "",
+            matcher: { type: "favourited", favourited: true },
+          };
+        case "not_fav":
+          return {
+            text: "",
+            matcher: { type: "favourited", favourited: false },
+          };
+        case "archived":
+          return {
+            text: "",
+            matcher: { type: "archived", archived: true },
+          };
+        case "not_archived":
+          return {
+            text: "",
+            matcher: { type: "archived", archived: false },
+          };
+        default:
+          // If the token is not known, emit it as pure text
+          return {
+            text: `is:${toks.text}`,
+            matcher: undefined,
+          };
+      }
+    }),
     apply(
       seq(
+        alt(tok(TokenType.Qualifier), tok(TokenType.Hash)),
         alt(
-          tok(TokenType.UrlPrefix),
-          tok(TokenType.TagPrefix),
-          tok(TokenType.ListPrefix),
+          apply(tok(TokenType.Ident), (tok) => {
+            return tok.text;
+          }),
+          apply(tok(TokenType.StringLiteral), (tok) => {
+            return tok.text.slice(1, -1);
+          }),
         ),
-        apply(tok(TokenType.StringLiteral), (tok) => {
-          return tok.text.slice(1, -1);
-        }),
       ),
       (toks) => {
-        switch (toks[0].kind) {
-          case TokenType.UrlPrefix:
-            return { type: "url", url: toks[1] };
-          case TokenType.TagPrefix:
-            return { type: "tagName", tagName: toks[1] };
-          case TokenType.ListPrefix:
-            return { type: "listName", listName: toks[1] };
-        }
-      },
-    ),
-    apply(
-      alt(
-        tok(TokenType.Url),
-        tok(TokenType.Tag),
-        tok(TokenType.List),
-        tok(TokenType.After),
-        tok(TokenType.Before),
-      ),
-      (toks) => {
-        switch (toks.kind) {
-          case TokenType.Url:
-            return { type: "url", url: toks.text.slice("url:".length) };
-          case TokenType.Tag:
-            return { type: "tagName", tagName: toks.text.slice("#".length) };
-          case TokenType.List:
+        switch (toks[0].text) {
+          case "url:":
             return {
-              type: "listName",
-              listName: toks.text.slice("list:".length),
+              text: "",
+              matcher: { type: "url", url: toks[1] },
             };
-          case TokenType.After:
+          case "#":
             return {
-              type: "dateAfter",
-              dateAfter: z.coerce
-                .date()
-                .parse(toks.text.slice("after:".length)),
+              text: "",
+              matcher: { type: "tagName", tagName: toks[1] },
             };
-          case TokenType.Before:
+          case "list:":
             return {
-              type: "dateBefore",
-              dateBefore: z.coerce
-                .date()
-                .parse(toks.text.slice("before:".length)),
+              text: "",
+              matcher: { type: "listName", listName: toks[1] },
+            };
+          case "after:":
+            return {
+              text: "",
+              matcher: {
+                type: "dateAfter",
+                dateAfter: z.coerce.date().parse(toks[1]),
+              },
+            };
+          case "before:":
+            return {
+              text: "",
+              matcher: {
+                type: "dateBefore",
+                dateBefore: z.coerce.date().parse(toks[1]),
+              },
+            };
+          default:
+            // If the token is not known, emit it as pure text
+            return {
+              text: toks[0].text + toks[1],
+              matcher: undefined,
             };
         }
       },
     ),
+    apply(seq(tok(TokenType.Ident), tok(TokenType.Space), MATCHER), (toks) => {
+      return {
+        text: [toks[0].text, toks[2].text].join(" ").trim(),
+        matcher: toks[2].matcher,
+      };
+    }),
+    apply(tok(TokenType.Ident), (toks) => {
+      return {
+        text: toks.text,
+        matcher: undefined,
+      };
+    }),
     kmid(tok(TokenType.LParen), EXP, tok(TokenType.RParen)),
   ),
 );
@@ -173,14 +250,72 @@ EXP.setPattern(
       switch (next[0].kind) {
         case TokenType.Space:
         case TokenType.And:
-          return { type: "and", matchers: [toks, next[1]] };
+          return {
+            text: [toks.text, next[1].text].join(" ").trim(),
+            matcher: {
+              type: "and",
+              matchers: [toks.matcher, next[1].matcher].filter(
+                (a) => !!a,
+              ) as Matcher[],
+            },
+          };
         case TokenType.Or:
-          return { type: "or", matchers: [toks, next[1]] };
+          return {
+            text: [toks.text, next[1].text].join(" ").trim(),
+            matcher: {
+              type: "or",
+              matchers: [toks.matcher, next[1].matcher].filter(
+                (a) => !!a,
+              ) as Matcher[],
+            },
+          };
       }
     },
   ),
 );
 
-export function parseSearchQuery(query: string): Matcher {
-  return expectSingleResult(expectEOF(EXP.parse(lexer.parse(query))));
+function flattenAndsAndOrs(matcher: Matcher): Matcher {
+  switch (matcher.type) {
+    case "and":
+    case "or": {
+      if (matcher.matchers.length == 1) {
+        return flattenAndsAndOrs(matcher.matchers[0]);
+      }
+      const flattened: Matcher[] = [];
+      for (let m of matcher.matchers) {
+        // If inside the matcher is another matcher of the same type, flatten it
+        m = flattenAndsAndOrs(m);
+        if (m.type == matcher.type) {
+          flattened.push(...m.matchers);
+        } else {
+          flattened.push(m);
+        }
+      }
+      matcher.matchers = flattened;
+      return matcher;
+    }
+    default:
+      return matcher;
+  }
+}
+
+export function _parseAndPrintTokens(query: string) {
+  console.log(`PARSING: ${query}`);
+  let tok = new Lexer().lex(query);
+  do {
+    console.log(tok?.kind, tok?.text);
+    tok = tok?.next;
+  } while (tok);
+  console.log("DONE");
+}
+
+export function parseSearchQuery(query: string): TextAndMatcher {
+  // _parseAndPrintTokens(query); // Uncomment to debug tokenization
+  const parsed = expectSingleResult(
+    expectEOF(EXP.parse(new Lexer().lex(query.trim()))),
+  );
+  if (parsed.matcher) {
+    parsed.matcher = flattenAndsAndOrs(parsed.matcher);
+  }
+  return parsed;
 }
