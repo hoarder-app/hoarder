3ac23c878c2e4a90b8fe937244c0ba227a744b51
Daniel Vigaru <22689627+danielvigaru@users.noreply.github.com>
2025-01-22 21:52:21 +0200

    fix: left margin for list items

--- apps/mobile/lib/utils.ts
+++ apps/mobile/lib/utils.ts
@@ -5,3 +5,39 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+interface ConditionalProps {
+  condition: boolean;
+  props: Record<string, unknown>;
+}
+/**
+ * Merge props conditionally.
+ *
+ * @example
+ * ```
+ * <View {...condProps(
+ *     { condition: true, props: { className: "foo" } },
+ *     { condition: true, props: { style: { margin: "10px" } } },
+ * )} />
+ * ```
+ * results in:
+ * ```
+ * <View className="foo" style={ margin: "10px" } />
+ * ```
+ * @example
+ * ```
+ * <View style={condProps(
+ *     { condition: true, color: "red" },
+ *     { condition: true, fontWeight: "bold" }
+ * )} />
+ * ```
+ * results in:
+ * ```
+ * <View style={ color: "red", fontWeight: "bold" } />
+ * ```
+ */
+export function condProps(...condProps: ConditionalProps[]) {
+  return condProps.reduce((acc, { condition, props }) => {
+    return condition ? { ...acc, ...props } : acc;
+  }, {});
+}
