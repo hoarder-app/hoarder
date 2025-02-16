d0bd638755b91b81563084d8cafa2d95a97e4fd3
Gavin Mogan <git@gavinmogan.com>
2025-02-15 19:56:58 -0800

    repair typescript types

--- packages/shared/customFetch.ts
+++ packages/shared/customFetch.ts
@@ -1,11 +1,14 @@
-import { Agent } from "undici";
 import serverConfig from "./config";
 
 // Custom fetch function with configurable timeout
-export const customFetch = (input: RequestInfo, init: RequestInit = {}) => {
+export function customFetch(
+  input: Parameters<typeof fetch>[0],
+  init?: Parameters<typeof fetch>[1],
+): ReturnType<typeof fetch> {
   const timeout = serverConfig.inference.fetchTimeoutSec * 1000; // Convert to milliseconds
   return fetch(input, {
+    signal: AbortSignal.timeout(timeout),
     ...init,
-    dispatcher: new Agent({ headersTimeout: timeout }),
   });
 };
+
