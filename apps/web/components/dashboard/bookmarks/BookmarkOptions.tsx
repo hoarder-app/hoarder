7407f6714e35892c38922cf98311c92281dbc74d
ekambains <bainseka@sheridancollege.ca>
2025-04-06 01:51:39 -0400

    ui(web): Now the copy link button is greyed out when clipboard is not available

--- apps/web/components/dashboard/bookmarks/BookmarkOptions.tsx
+++ apps/web/components/dashboard/bookmarks/BookmarkOptions.tsx
@@ -48,6 +48,7 @@ export default function BookmarkOptions({ bookmark }: { bookmark: ZBookmark }) {
   const linkId = bookmark.id;
 
   const demoMode = !!useClientConfig().demoMode;
+  const isClipboardAvailable = !!navigator.clipboard;
 
   const { setOpen: setTagModalIsOpen, content: tagModal } =
     useTagModel(bookmark);
@@ -189,6 +190,7 @@ export default function BookmarkOptions({ bookmark }: { bookmark: ZBookmark }) {
 
           {bookmark.content.type === BookmarkTypes.LINK && (
             <DropdownMenuItem
+              disabled={!isClipboardAvailable}
               onClick={() => {
                 navigator.clipboard.writeText(
                   (bookmark.content as ZBookmarkedLink).url,
