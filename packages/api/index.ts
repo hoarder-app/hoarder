dbdd263403e9b71ccf92ffba8a704634ffcfbd7c
Mohamed Bassem <me@mbassem.com>
2025-05-17 18:48:32 +0000

    Implement the bookmarks and users endpoints

--- packages/api/index.ts
+++ packages/api/index.ts
@@ -2,9 +2,18 @@ import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { poweredBy } from "hono/powered-by";
 
+import bookmarks from "./routes/bookmarks";
 import highlights from "./routes/highlights";
+import lists from "./routes/lists";
+import tags from "./routes/tags";
+import users from "./routes/users";
 
-const v2 = new Hono().route("/highlights", highlights);
+const v2 = new Hono()
+  .route("/highlights", highlights)
+  .route("/bookmarks", bookmarks)
+  .route("/lists", lists)
+  .route("/tags", tags)
+  .route("/users", users);
 
 const app = new Hono().use(logger()).use(poweredBy()).route("/v2", v2);
 
