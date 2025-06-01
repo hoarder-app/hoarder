c0889ca348655da7b143b104af9e09f00d27ebb4
Mohamed Bassem <me@mbassem.com>
2025-06-01 15:23:48 +0000

    Correctly set page title

--- apps/web/app/public/lists/[listId]/page.tsx
+++ apps/web/app/public/lists/[listId]/page.tsx
@@ -1,5 +1,21 @@
+import type { Metadata } from "next";
 import PublicLists from "@/components/public/lists/PublicLists";
 import { api } from "@/server/api/client";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { listId: string };
+}): Promise<Metadata> {
+  // TODO: Don't load the entire list, just create an endpoint to get the list name
+  const resp = await api.publicBookmarks.getPublicBookmarksInList({
+    listId: params.listId,
+  });
+
+  return {
+    title: `${resp.list.name} - Karakeep`,
+  };
+}
 
 export default async function PublicListPage({
   params,
@@ -11,18 +27,16 @@ export default async function PublicListPage({
   });
 
   return (
-    <>
-      <PublicLists
-        bookmarks={resp.bookmarks}
-        list={{
-          id: params.listId,
-          name: resp.list.name,
-          description: resp.list.description,
-          icon: resp.list.icon,
-          numItems: resp.list.numItems,
-        }}
-        nextCursor={resp.nextCursor}
-      />
-    </>
+    <PublicLists
+      bookmarks={resp.bookmarks}
+      list={{
+        id: params.listId,
+        name: resp.list.name,
+        description: resp.list.description,
+        icon: resp.list.icon,
+        numItems: resp.list.numItems,
+      }}
+      nextCursor={resp.nextCursor}
+    />
   );
 }
