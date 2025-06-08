06723baed5ecd3d3267227718129e6b5d16741c2
MohamedBassem <me@mbassem.com>
2025-06-08 20:02:49 +0100

    Attempt upgrade nextjs

--- apps/web/app/api/auth/[...nextauth]/route.tsx
+++ apps/web/app/api/auth/[...nextauth]/route.tsx
@@ -1,3 +1,5 @@
 import { authHandler } from "@/server/auth";
 
-export { authHandler as GET, authHandler as POST };
+export { /* @next-codemod-error `authHandler` export is re-exported. Check if this component uses `params` or `searchParams`*/
+authHandler as GET, /* @next-codemod-error `authHandler` export is re-exported. Check if this component uses `params` or `searchParams`*/
+authHandler as POST };
